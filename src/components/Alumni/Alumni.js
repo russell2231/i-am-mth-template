1653dba7ac4294367d3aec7d22e3b8726a0b4368
Russell Rice <russellrice15@gmail.com>
2022-01-31 00:50:33 -0500

    Style alumni component for medium sized devices

--- src/components/Alumni/Alumni.js
+++ src/components/Alumni/Alumni.js
@@ -53,6 +53,16 @@ const Alumni = () => {
 							View Profile
 						</Link>
 					</div>
+					<div className={styles.alum}>
+						<img className={styles.alumImg} src={profileImg} alt='profile' />
+						<h2 className={styles.alumName}>Russell Rice</h2>
+						<p className={styles.alumCollege}>University of Akron</p>
+						<p className={styles.alumJob}>Front End Web Developer</p>
+						<p className={styles.alumClass}>Class of 2014</p>
+						<Link className={styles.btn} to='alumni/$name'>
+							View Profile
+						</Link>
+					</div>
 				</div>
 			</div>
 		</main>
